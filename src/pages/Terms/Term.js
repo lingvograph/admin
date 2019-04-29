60440fd0e247845e2cf34b658aed2638904eb49e
sergeyt <stodyshev@gmail.com>
2019-04-29 08:00:10 +0700

    TermSlider -> TermCarousel

--- src/pages/Terms/Term.js
+++ src/pages/Terms/Term.js
@@ -5,7 +5,7 @@ import * as api from 'api';
 import Loading from 'components/Loading';
 import FormCard from 'components/FormCard';
 import TagsCard from 'components/TagsCard';
-import TermSlider from './TermSlider';
+import TermCarousel from './TermCarousel';
 
 const fields = [
   {
@@ -55,7 +55,7 @@ export const Term = () => {
               <strong>Visual</strong>
             </CardHeader>
             <CardBody>
-              <TermSlider term={term}/>
+              <TermCarousel term={term}/>
             </CardBody>
           </Card>
         </Col>
