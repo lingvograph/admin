b8a09893b221fd3722255155504b0862f108d6c0
sergeyt <stodyshev@gmail.com>
2019-05-01 00:06:01 +0700

    search icon

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Input } from 'reactstrap';
+import { InputGroup, InputGroupAddon, Input, Button } from 'reactstrap';
 import { useSearchParams } from 'hooks';
 
 const SearchInput = () => {
@@ -9,7 +9,14 @@ const SearchInput = () => {
 
   const handleChange = e => replaceParam(paramKey, e.target.value);
 
-  return <Input type="text" placeholder="Search by text" value={searchString} onChange={handleChange} />;
+  return (
+    <InputGroup>
+      <Input type="text" placeholder="Search by text" value={searchString} onChange={handleChange} />
+      <InputGroupAddon addonType="append">
+        <Button type="button" color="primary"><i className="fa fa-search"></i></Button>
+      </InputGroupAddon>
+    </InputGroup>
+  );
 };
 
 export default SearchInput;
