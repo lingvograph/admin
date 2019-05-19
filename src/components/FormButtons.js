ceb13adc4dcaf2a9519523478c5918f5cb447e3f
sergeyt <stodyshev@gmail.com>
2019-05-20 00:57:39 +0700

    chore: auto fmt

--- src/components/FormButtons.js
+++ src/components/FormButtons.js
@@ -5,7 +5,14 @@ export const SubmitButton = ({ formikProps, label = 'Save', ...buttonProps }) =>
   const { dirty, isSubmitting, handleSubmit } = formikProps;
 
   return (
-    <Button color="primary" size="sm" {...buttonProps} type="submit" disabled={!dirty || isSubmitting} onClick={handleSubmit}>
+    <Button
+      color="primary"
+      size="sm"
+      {...buttonProps}
+      type="submit"
+      disabled={!dirty || isSubmitting}
+      onClick={handleSubmit}
+    >
       <span>{label}</span>
     </Button>
   );
@@ -15,7 +22,14 @@ export const ResetButton = ({ formikProps, label = 'Reset', ...buttonProps }) =>
   const { dirty, isSubmitting, handleReset } = formikProps;
 
   return (
-    <Button color="danger" size="sm" {...buttonProps} type="reset" disabled={!dirty || isSubmitting} onClick={handleReset}>
+    <Button
+      color="danger"
+      size="sm"
+      {...buttonProps}
+      type="reset"
+      disabled={!dirty || isSubmitting}
+      onClick={handleReset}
+    >
       <span>{label}</span>
     </Button>
   );
